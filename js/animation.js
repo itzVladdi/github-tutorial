13015ac5eb160132e58fef4485ccab86ebd85c28
Vladislav Marinin <czargion@gmail.com>
2022-11-08 17:54:13 +0200

    123

---
+++ js/animation.js
@@ -0,0 +1,7 @@
+const buttonSwitch = document.querySelector('.btn');
+const textOne = document.querySelector('one');
+const textTwo = document.querySelector('two')
+
+buttonSwitch.addEventListener("click", () => {
+    buttonSwitch.classList.toggle()
+} )
